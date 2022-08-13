a81ec9653daa1d61cb8c16569876c16cfc8e6eca
Yasz Avellia <zakamaragames@gmail.com>
2022-08-13 21:08:57 +0700

    memasukan bootstrap cdn

--- login3.php
+++ login3.php
@@ -9,6 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
 </head>
 <body>
     <form action="" method="post">
@@ -16,5 +18,11 @@
         <input type="password" name="pw" placeholder="Masukan Password Anda" >
         <button type="submit" name="sbt">Masuk</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script>
+        if ( window.history.replaceState ) {
+            window.history.replaceState( null, null, window.location.href );
+            }
+    </script>
 </body>
 </html>
