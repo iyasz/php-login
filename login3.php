3febeb29a991b7311aa6f5227d2d293676f9741c
Yasz Avellia <zakamaragames@gmail.com>
2022-08-14 07:01:52 +0700

    membuat form

--- login3.php
+++ login3.php
@@ -1,6 +1,6 @@
 <?php
 
-
+$conn = mysqli_connect('localhost', 'root', '', 'db_logintest');
 
 ?>
 
@@ -15,6 +15,11 @@
 
 </head>
 <body>
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="Masukan Username">
+        <input type="password" name="pw" placeholder="Masukan Password">
+        <button class="btn btn-primary" type="submit" name="btn-log" >Login</button>
+    </form>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script>
