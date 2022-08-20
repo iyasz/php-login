1675e5d68de09b5ae5d9a700a39e2ecd475c1463
Yasz Avellia <zakamaragames@gmail.com>
2022-08-21 02:35:00 +0700

    membuat card pada login

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 $conn = mysqli_connect('localhost', 'root', '', 'db_logintest');
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $username = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['pw']);
-    if($username == "" or $password == ""){
+    if ($username == "" or $password == "") {
         $alert = "Masukan Email Dan Password";
     }
 }
@@ -15,6 +15,7 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,23 +23,40 @@ if(isset($_POST['submit'])){
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
+
 <body>
-    <form action="" method="post">
-        <input type="text" name="email" placeholder="Masukan Email Anda" >
-        <input type="password" name="pw" placeholder="Masukan Password Anda">
-        <p><?php if(isset($alert)) {
-            echo "$alert";
-        } ?></p>
-        <button class="btn btn-primary" type="masuk" name="submit">Masuk</button>
-    </form>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-xl-6">
+                <div class="card shadow-lg">
+                    <div class="card-header text-bg-dark ps-3">
+                        <h1>Login</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <label for="username"></label>
+                            <input type="text" id="username" class="form-control" name="email" placeholder="Masukan Email Anda">
+                            <input type="password" name="pw" placeholder="Masukan Password Anda">
+                            <p><?php if (isset($alert)) {
+                                    echo "$alert";
+                                } ?></p>
+                            <button class="btn btn-primary" type="masuk" name="submit">Masuk</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script>
-        if ( window.history.replaceState ) {
-            window.history.replaceState( null, null, window.location.href );
-            }
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
     </script>
 
 </body>
+
 </html>
